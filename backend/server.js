f65bf1ecc634aef645b34e130ea4bb54b767e3dc
Mario Torres <mario.torres42@yahoo.com>
2025-04-29 22:10:12 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const { Sequelize, DataTypes } = require("sequelize");
 const { Pool } = require('pg');
 
 const app = express();
-const PORT = process.env.PORT || 5009;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
